b97b92a3059bf026b59c26baff4b133dc0576b6f
DerAngler <florian.angler1@googlemail.com>
2023-10-27 05:19:21 +0200

    GUI fertig (Log-Fenster nur halb)

--- EndpointCreationTool.ps1
+++ EndpointCreationTool.ps1
@@ -1,7 +1,9 @@
 [System.Reflection.Assembly]::LoadWithPartialName("PresentationFramework") | Out-Null
 
+$Version = "v1.1.0"
+
 function Import-Xaml {
-	[xml]$xaml = Get-Content -Path $PSScriptRoot\WPF_Vorlage.xaml -Encoding UTF8
+	[xml]$xaml = Get-Content -Path $PSScriptRoot\EndpointCreationTool.xaml -Encoding UTF8
 	$manager = New-Object System.Xml.XmlNamespaceManager -ArgumentList $xaml.NameTable
 	$manager.AddNamespace("x", "http://schemas.microsoft.com/winfx/2006/xaml");
 	$xamlReader = New-Object System.Xml.XmlNodeReader $xaml
@@ -9,4 +11,24 @@ function Import-Xaml {
 }
 
 $Window = Import-Xaml
+
+$Standort_CoBo = $Window.FindName('Standort_CoBo')
+$Typ_CoBo = $Window.FindName('Typ_CoBo')
+$Lizenz_CoBo = $Window.FindName('Lizenz_CoBo')
+$Benutzer_TB = $Window.FindName('Benutzer_TB')
+$Abteilung_TB = $Window.FindName('Abteilung_TB')
+$NDL_TB = $Window.FindName('NDL_TB')
+$MAC_TB = $Window.FindName('MAC_TB')
+$OSErlauben_CB = $Window.FindName('OSErlauben_CB')
+$OSZuweisen_CB = $Window.FindName('OSZuweisen_CB')
+$ClientAnlegen_BTN = $Window.FindName('ClientAnlegen_BTN')
+$RichtigeOU_CB = $Window.FindName('RichtigeOU_CB')
+$Log_RTB = $Window.FindName('Log_RTB')
+
+$Log_RTB.AppendText("Endpoint Creation Tool $($Version) gestartet`rWarte auf Eingaben...`r")
+
+$ClientAnlegen_BTN.Add_Click({
+	$Log_RTB.AppendText("MAC-Adresse: $($MAC_TB.Text)")
+})
+
 $Window.ShowDialog()
